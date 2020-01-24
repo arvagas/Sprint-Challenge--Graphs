ae8b74e91fab55457075c5a53fbe29525ba7a4d0
Arvin Agas <arvagas@gmail.com>
2020-01-24 06:53:10 -0500

    traversing with just over 1000 moves

--- adv.py
+++ adv.py
@@ -1,14 +1,34 @@
 from room import Room
 from player import Player
 from world import World
-from queue import Queue
+from util import Queue, Stack
 
 import random
 from ast import literal_eval
 
+def bfs(starting_room_id):
+    queue = Queue()
+    queue.enqueue([starting_room_id])
+    visited = set()
+
+    while queue.size() > 0:
+        # grab path
+        path = queue.dequeue()
+        # take last in path
+        current_room = path[-1]
+        visited.add(current_room)
+
+        for direction in checked_rooms_dict[current_room]:
+            if checked_rooms_dict[current_room][direction] == '?':
+                return path    
+            elif checked_rooms_dict[current_room][direction] not in visited:
+                # create a new path to append direction
+                new_path = list(path)
+                new_path.append(checked_rooms_dict[current_room][direction])
+                queue.enqueue(new_path)
+
 # Load world
 world = World()
-
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
@@ -27,10 +47,68 @@ world.print_rooms()
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
-# traversal_path = ['n', 'n']
-traversal_path = []
+traversal_path = list()
 
+# Will hold all data of rooms checked
+checked_rooms_dict = dict()
 
+rev_dir = {'n':'s', 's':'n', 'e':'w', 'w':'e'}
+prev_room_id = 0
+
+while len(checked_rooms_dict) != len(room_graph):
+    current_room = player.current_room
+    room_id = current_room.id
+    crd = dict() # current_room_dictonary
+
+    # Check to see if player has explored the room already
+    if room_id not in checked_rooms_dict:
+        # Record exits and label as '?'
+        for passage in current_room.get_exits():
+            crd[passage] = '?'
+        # Update with previous room id
+        if traversal_path:
+            prev_dir = rev_dir[traversal_path[-1]]
+            crd[prev_dir] = prev_room_id
+        # Update overall dictonary
+        checked_rooms_dict[room_id] = crd
+    # If it already exists, grab data from overall dictionary
+    else:
+        crd = checked_rooms_dict[room_id]
+
+    # Check to see if there are still unknown rooms connected
+    unknown_exits = list()
+    for direction in crd:
+        if crd[direction] == '?':
+            unknown_exits.append(direction)
+
+    # If unknowns exist, go in one of the directions
+    if len(unknown_exits) != 0:
+        direction = unknown_exits[0]
+        traversal_path.append(direction)
+        player.travel(direction)
+        # Update the ?'s
+        room_move = player.current_room
+        checked_rooms_dict[current_room.id][direction] = room_move.id
+        prev_room_id = current_room.id
+    # Otherwise, find a way back to closest room with an unknown exit
+    else:
+        # Find closest room via BFS
+        path_to_next = bfs(room_id)
+
+        # Make sure there's actually something being returned
+        if path_to_next is not None and len(path_to_next) > 0:
+            # Have the player travel back to room with unknown exits
+            for index in range(len(path_to_next) - 1):
+                for direction in checked_rooms_dict[path_to_next[index]]:
+                    if checked_rooms_dict[path_to_next[index]][direction] == path_to_next[index + 1]:
+                        traversal_path.append(direction)
+                        player.travel(direction)
+        else:
+            break
+                    
+
+print(traversal_path)
+print(checked_rooms_dict)
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -52,12 +130,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
